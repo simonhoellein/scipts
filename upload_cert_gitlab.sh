bda7049bd38228bc11e8fe00678fc18e97161734
shoellein <simonhoellein@gmail.com>
2022-06-04 19:09:01 +0200

    readme information update

--- upload_cert_gitlab.sh
+++ upload_cert_gitlab.sh
@@ -11,7 +11,6 @@ WD=/tmp/cert-script/live
 DIR_NUM=/tmp/cert-script/dir_num
 
 # Intro-Text
-<<<<<<< HEAD
 echo "_________                __  .__  _____.__               __                 "
 echo "\_   ___ \  ____________/  |_|__|/ ____\__| ____ _____ _/  |_  ____   ______"
 echo "/    \  \/_/ __ \_  __ \   __\  \   __\|  |/ ___\\__  \\   __\/ __ \ /  ___/"
@@ -27,23 +26,6 @@ echo "Working Directory: $WD"
 echo "Directory-Numbers: $DIR_NUM"
 echo ""
 sleep 2
-=======
-    echo "_________                __  .__  _____.__               __                 "
-    echo "\_   ___ \  ____________/  |_|__|/ ____\__| ____ _____ _/  |_  ____   ______"
-    echo "/    \  \/_/ __ \_  __ \   __\  \   __\|  |/ ___\\__  \\   __\/ __ \ /  ___/"
-    echo "\     \___\  ___/|  | \/|  | |  ||  |  |  \  \___ / __ \|  | \  ___/ \___ \ "
-    echo " \______  /\___  >__|   |__| |__||__|  |__|\___  >____  /__|  \___  >____  >"
-    echo "        \/     \/                              \/     \/          \/     \/ "
-    echo ""
-    echo "# Script by sh"
-    echo ""
-    echo "# Explenation: This Script is ment to upload Certificates from NGINX Proxy Manager to a Gitlab Repository"
-    echo ""
-    echo "Working Directory: $WD"
-    echo "Directory-Numvers: $DIR_NUM"
-    echo ""
-    sleep 2
->>>>>>> 0c9ad9aaee8bdf5b1a0f31938e4069879fa5bd59
 # Copy Certfiles to Work-Directory and rm all README files
 echo ""
 echo "###############################################################################"
@@ -85,11 +67,11 @@ do
         exp_date=$(openssl x509 -in $WD/npm-$i/cert.pem -noout -text | grep "Not After :" | cut -d ":" -f 2- | cut -c 2-)
         echo "# Information about $cn" > $WD/npm-$i/README.md
         echo "--------" >> $WD/npm-$i/README.md
-        echo "Experiation Date: $exp_date" >> $WD/npm-$i/README.md
+        echo "Experiation Date:" $exp_date >> $WD/npm-$i/README.md
         echo "## Other Information: " >> $WD/npm-$i/README.md
-        echo "```" >> $WD/npm-$i/README.md
+        echo "````" >> $WD/npm-$i/README.md
         openssl x509 -in $WD/npm-$i/cert.pem -noout -text >> $WD/npm-$i/README.md
-        echo "```" >> $WD/npm-$i/README.md
+        echo "````" >> $WD/npm-$i/README.md
         mv $WD/npm-$i $WD/$cn
         echo "Certificate Nr. $i"
         echo "======"
